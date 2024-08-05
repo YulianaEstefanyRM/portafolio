338c8a7e0b484fac07116a6e7ce68ea22eb7e0b7
Yuliana Stefany Rosero Morales <yulianaroserom@gmail.com>
2024-08-04 20:00:12 -0500

    Cambios domingo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'estefany';
+  @ViewChild('cursor') cursor!: ElementRef<HTMLDivElement>;
+
+  constructor(private el: ElementRef) {}
+
+  // Método para mover el cursor
+  @HostListener('mousemove', ['$event'])
+  onMouseMove(event: MouseEvent) {
+    const cursorElement = this.cursor.nativeElement;
+    cursorElement.style.left = `${event.pageX}px`;
+    cursorElement.style.top = `${event.pageY}px`;
+  }
+
+  // Cambia el tamaño del cursor al pasar sobre un botón
+  onButtonHover(hover: boolean) {
+    const cursorElement = this.cursor.nativeElement;
+    cursorElement.style.transform = hover ? 'scale(1.5)' : 'scale(1)';
+    cursorElement.style.backgroundColor = hover ? '#333' : 'transparent'; // Cambia el color
+  }
+
 }
